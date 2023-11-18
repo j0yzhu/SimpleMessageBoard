bea9d5da40bc6a04f8e49d42c27a541f3620ca1d
eli <eli.chandler@gmail.com>
2023-11-18 23:35:39 +1300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, Response, render_template, session, redirect
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "jfurehfieo"
+
+# message will have an author and a content
+class Message:
+    def __init__(self, author, content):
+        self.author = author
+        self.content = content
+
+
+messages = []
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    return render_template("home.html", messages=messages)
+
+# endpoint for sending messages
+@app.route("/message", methods=['POST'])
+def send_message():
+    author = request.form.get('author')
+    content = request.form.get('content')
+
+    print(author)
+    print(content)
+
+    messages.append(Message(author, content))
+    return redirect('/')
+
+# endpoint for getting a list of messages
+@app.route("/message", methods=['GET'])
+def get_response():
+    return {
+        'messages': messages
+    }
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
